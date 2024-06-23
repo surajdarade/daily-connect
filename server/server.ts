0cc331c81d4303c0ac0625e89affbb1eaf506735
Suraj Narayan Darade <daradesuraj05@gmail.com>
2024-06-23 18:19:01 +0530

    server start file

---
+++ server/server.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import connectToDatabase from "./config/database";
+import authRouter from "./routes/authRoutes";
+import userRouter from "./routes/userRoutes";
+import chatRouter from "./routes/chatRoutes";
+
+const app = express();
+dotenv.config();
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URI!,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 8000;
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/chat", chatRouter);
+
+connectToDatabase().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on PORT: ${PORT} 🚀`);
+  });
+});
