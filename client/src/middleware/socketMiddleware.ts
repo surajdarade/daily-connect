ec4357914a03188b878902426da32a9a5c0d16ab
Suraj Narayan Darade <daradesuraj05@gmail.com>
2024-06-28 11:15:53 +0530

    socket middleware

---
+++ client/src/middleware/socketMiddleware.ts
@@ -0,0 +1,45 @@
+import { Middleware } from "@reduxjs/toolkit";
+import { io, Socket } from "socket.io-client";
+import { setOnlineUser, userSliceReset } from "../store/userSlice";
+
+let socket: Socket | null = null;
+
+const isActionWithType = (action: unknown): action is { type: string } => {
+  return typeof action === "object" && action !== null && "type" in action && typeof (action as { type: unknown }).type === "string";
+};
+
+export const socketMiddleware: Middleware = ({ dispatch }) => (next) => (action) => {
+  if (!isActionWithType(action)) {
+    return next(action);
+  }
+
+  if (action.type === "user/connectSocket") {
+    const token = localStorage.getItem("token");
+    if (token) {
+      socket = io("http://localhost:3000", {
+        auth: {
+          token,
+        },
+      });
+
+      socket.on("onlineUser", (data) => {
+        dispatch(setOnlineUser(data));
+      });
+
+      socket.on("disconnect", () => {
+        socket = null;
+      });
+    }
+  }
+
+  if (action.type === userSliceReset.type) {
+    if (socket) {
+      socket.disconnect();
+      socket = null;
+    }
+  }
+
+  return next(action);
+};
+
+export const getSocket = () => socket;
