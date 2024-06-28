56f286aee4006f3f30f6bc35b120cf5a938416ce
Suraj Narayan Darade <daradesuraj05@gmail.com>
2024-06-28 11:21:11 +0530

    persisted store

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,18 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/ClientRoutes";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+
+const persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <Provider store={store}>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <RouterProvider router={router} />
-    </Provider>
-  </React.StrictMode>
+    </PersistGate>
+  </Provider>
 );
